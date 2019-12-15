866cf474cfa6c6bbefd1f82644b84cf72a672b8a
david <david.bingjian.yu>
2019-12-15 10:45:09 +0800

    Initial version of aimee generated by JHipster-6.5.1

---
+++ src/main/java/com/dystudio/aimee/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.dystudio.aimee.repository.search;
---
+++ src/main/java/com/dystudio/aimee/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.dystudio.aimee.service;
